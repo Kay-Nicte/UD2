4d5ab79f42593c86ba605df68f90b83ee7cca707
Ixabel Justo <71872946+Kay-Nicte@users.noreply.github.com>
2022-04-06 00:12:11 +0200

    Initial commit

---
+++ src/ComentarioLinea.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+
+/**
+ * @author x
+ *
+ */
+public class ComentarioLinea {
+
+	/**
+	 * 
+	 */
+	public ComentarioLinea() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//System.out.println("Hola");
+		System.out.println("Adios");
+		System.out.println("FIN DE PROGRAMA");
+
+	}
+
+}
